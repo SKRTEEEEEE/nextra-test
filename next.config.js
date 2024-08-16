cdc7cc50a659f0a3387fb46b27951121775c51d9
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-16 13:07:48 +0200

    fix(app): Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+};
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra(nextConfig);
