d1696f6e90027c9a7b3528e4c406cfe06606a06b
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-16 12:52:49 +0200

    template(app): Template app pages router

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+// import "@/app/ui/global.css"
+// import { inter } from "../components/ui/fonts";
+ 
+// export const metadata: Metadata = {
+//   title: {
+//     template: '%s | Acme Dashboard',
+//     default: 'Acme Dashboard',
+//   },
+//   description: 'The official Next.js Learn Dashboard built with App Router.',
+//   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+// };
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
